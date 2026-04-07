2582e33da3ece1df1b07ccb1b978d025d6977fa1
Momina Arshad <mominaarshad@github.com>
2026-04-07 14:30:54 +0200

    Convert project to Typecript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Hello React + TypeScript </h1>
+      <p>My pilot project now uses TypeScript!</p>
+    </div>
+  );
+}
+
+export default App;
