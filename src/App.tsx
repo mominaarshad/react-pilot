7b48fb385ebddb8a1ad8698446b2f6e084f68678
Momina Arshad <mominaarshad@github.com>
2026-04-07 14:36:28 +0200

    Added a disabled button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-function MyButton({ title }: { title: string }) {
+interface MyButtonProps {
+  /** The text to display inside the button */
+  title: string;
+  /** Whether the button can be interacted with */
+  disabled: boolean;
+}
+
+function MyButton({ title, disabled }: MyButtonProps) {
   return (
-    <button>{title}</button>
+    <button disabled={disabled}>{title}</button>
   );
 }
 
@@ -8,7 +15,7 @@ export default function MyApp() {
   return (
     <div>
       <h1>Welcome to my app</h1>
-      <MyButton title="I'm a button" />
+      <MyButton title="I'm a disabled button" disabled={true}/>
     </div>
   );
 }
